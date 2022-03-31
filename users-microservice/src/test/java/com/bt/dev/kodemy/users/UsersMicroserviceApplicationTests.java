b39a90086b291ea80e9426e9b8777922ad498c20
Christian Kabore <chriskabor@gmail.com>
2022-03-31 15:10:34 +0000

    added datasource config

--- users-microservice/src/test/java/com/bt/dev/kodemy/users/UsersMicroserviceApplicationTests.java
+++ users-microservice/src/test/java/com/bt/dev/kodemy/users/UsersMicroserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.bt.dev.kodemy.users;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {UsersMicroserviceApplication.class})
 class UsersMicroserviceApplicationTests {
 
     @Test
