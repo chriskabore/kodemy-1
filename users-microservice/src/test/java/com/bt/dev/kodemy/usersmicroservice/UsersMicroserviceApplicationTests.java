0e44140308f389bee16502ea551e3016ec4e25a2
Christian Kabore <chriskabor@gmail.com>
2022-03-31 14:46:45 +0000

    initial commit

---
+++ users-microservice/src/test/java/com/bt/dev/kodemy/usersmicroservice/UsersMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bt.dev.kodemy.usersmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
